bf38a10797cbcd854e44f12ab81f007306d9e17e
Dmitriy Polskoy <polskoy@gmail.com>
2016-01-11 12:53:56 -0800

    Initial Commit

---
+++ tapper-extreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper-extreme
+//
+//  Created by Dmitriy Polskoy on 2016-01-11.
+//  Copyright © 2016 Dmitriy Polskoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
